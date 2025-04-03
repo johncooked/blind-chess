4be4b13170b60f45c49cbc60a806f454c1c0a8f7
johncooked <152047905+johncooked@users.noreply.github.com>
2025-04-03 21:46:26 +1000

    remove unused Vite logo and simplify App component return

--- chess-game/src/App.tsx
+++ chess-game/src/App.tsx
@@ -2,15 +2,7 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-    return (
-        <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-            </div>
-        </>
-    );
+    return <></>;
 }
 
 export default App;
