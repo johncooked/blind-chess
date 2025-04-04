890502c6b3c89dc4f502897ffe9160d1fda69a5b
johncooked <152047905+johncooked@users.noreply.github.com>
2025-04-04 20:39:26 +1000

    update side container in App component to include chat

--- chess-game/src/App.tsx
+++ chess-game/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
     return (
         <>
             <div className="main-container">chessboard</div>
-            <div className="side-container"></div>
+            <div className="side-container">chat</div>
         </>
     );
 }
